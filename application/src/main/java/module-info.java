0f85d11e66d2452407e0fbb3bcfbc59aaaea5ddb
Carl-Philipp Harmant <cp.harmant@gmail.com>
2018-01-12 12:31:52 -0600

    Add spring to application module

--- application/src/main/java/module-info.java
+++ application/src/main/java/module-info.java
@@ -1,3 +1,11 @@
 module cph.crypto.application {
 	requires cph.crypto.core;
+
+	requires spring.beans;
+	requires spring.core;
+	requires spring.context;
+	requires spring.web;
+	requires spring.webmvc;
+	requires spring.boot;
+	requires spring.boot.autoconfigure;
 }
