5563f6bd9240cad7ea2e667c568276abe960f0ae
Carl-Philipp Harmant <cp.harmant@gmail.com>
2018-01-16 09:48:28 -0600

    Add mongo share value adaptetr

---
+++ persistence/src/main/java/org/cph/crypto/persistence/MongoShareValueAdapter.java
@@ -0,0 +1,33 @@
+package org.cph.crypto.persistence;
+
+import org.cph.crypto.core.entity.ShareValue;
+import org.cph.crypto.core.entity.User;
+import org.cph.crypto.persistence.entity.ShareValueDb;
+import org.cph.crypto.persistence.entity.UserDb;
+import org.cph.crypto.persistence.repository.ShareValueRepository;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+public final class MongoShareValueAdapter implements org.cph.crypto.core.spi.ShareValueRepository {
+	private final ShareValueRepository repository;
+
+	public MongoShareValueAdapter(final ShareValueRepository repository) {
+		this.repository = repository;
+	}
+
+	public List<ShareValue> findAllByUser(final User user) {
+		return repository.findAllByUser(UserDb.from(user))
+			.stream()
+			.map(ShareValueDb::toShareValue)
+			.collect(Collectors.toList());
+	}
+
+	public Optional<ShareValue> findTop1ByUserOrderByTimestampDesc(final User user) {
+		return repository.findTop1ByUserOrderByTimestampDesc(UserDb.from(user)).map(ShareValueDb::toShareValue);
+	}
+
+	public ShareValue save(final ShareValue shareValue) {
+		return this.repository.save(ShareValueDb.from(shareValue)).toShareValue();
+	}
+}
--- persistence/src/main/java/org/cph/crypto/persistence/entity/ShareValueDb.java
+++ persistence/src/main/java/org/cph/crypto/persistence/entity/ShareValueDb.java
@@ -1,5 +1,6 @@
 package org.cph.crypto.persistence.entity;
 
+import org.cph.crypto.core.entity.ShareValue;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.DBRef;
@@ -10,8 +11,79 @@ public final class ShareValueDb {
 	@Id
 	private String id;
 	private Long timestamp;
-	@Indexed @DBRef UserDb user;
+	@Indexed
+	@DBRef
+	UserDb user;
 	private Double shareQuantity;
 	private Double shareValue;
 	private Double portfolioValue;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public Long getTimestamp() {
+		return timestamp;
+	}
+
+	public void setTimestamp(Long timestamp) {
+		this.timestamp = timestamp;
+	}
+
+	public UserDb getUser() {
+		return user;
+	}
+
+	public void setUser(UserDb user) {
+		this.user = user;
+	}
+
+	public Double getShareQuantity() {
+		return shareQuantity;
+	}
+
+	public void setShareQuantity(Double shareQuantity) {
+		this.shareQuantity = shareQuantity;
+	}
+
+	public Double getShareValue() {
+		return shareValue;
+	}
+
+	public void setShareValue(Double shareValue) {
+		this.shareValue = shareValue;
+	}
+
+	public Double getPortfolioValue() {
+		return portfolioValue;
+	}
+
+	public void setPortfolioValue(Double portfolioValue) {
+		this.portfolioValue = portfolioValue;
+	}
+
+	public ShareValue toShareValue() {
+		final ShareValue shareValue = new ShareValue();
+		shareValue.setId(id);
+		shareValue.setTimestamp(timestamp);
+		shareValue.setUser(user.toUser());
+		shareValue.setShareValue(this.shareValue);
+		shareValue.setShareQuantity(shareQuantity);
+		shareValue.setPortfolioValue(portfolioValue);
+		return shareValue;
+	}
+
+	public static ShareValueDb from(final ShareValue shareValue) {
+		final ShareValueDb shareValueDb = new ShareValueDb();
+		shareValueDb.setId(shareValue.getId());
+		shareValueDb.setTimestamp(shareValue.getTimestamp());
+		shareValueDb.setUser(UserDb.from(shareValue.getUser()));
+		shareValueDb.setShareValue(shareValue.getShareValue());
+		shareValueDb.setPortfolioValue(shareValue.getPortfolioValue());
+		return shareValueDb;
+	}
 }
